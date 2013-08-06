87b1858112b66b108b7b7b1d6e86fe620a66fb38
manonthemat <empower@manonthemat.com>
2013-08-05 18:14:35 -0700

    Added function get_all for getting all leads of both funnels and storing unique emails in emails.txt

--- src/ezmn_parser.py
+++ src/ezmn_parser.py
@@ -13,13 +13,14 @@ You don't have an account yet? Sign up here: http://ezmoneynetwork.com/signup.ph
 import requests # doc: http://docs.python-requests.org/
 
 from login_info import login_info
+from sys import argv
 
 # funnel_ids
 EZMF = 1
 EZMN = 5
 
 # lead_types
-# TODO: The HTML document for members is formatted differently. Parsing for email-addresses does not work yet
+# TODO: The HTML document for members is formatted differently. Parsing for email-addresses there does not work yet
 LEADS = 1
 MEMBERS = 2
 
@@ -27,9 +28,9 @@ def get_emails(funnel=EZMN, lead_type=LEADS):
     payload = {'username': login_info['user'], 'password': login_info['password']}
     p2 = {'funnel_id': funnel, 'lead_type': lead_type}
     
-    if funnel == EZMN:
+    if funnel == EZMN or funnel == 'EZMN':
         funnelname = 'EZMN'
-    elif funnel == EZMF:
+    elif funnel == EZMF or funnel == 'EZMF':
         funnelname = 'EZMF'
     else:
         funnelname = 'emails'
@@ -57,9 +58,47 @@ def get_emails(funnel=EZMN, lead_type=LEADS):
                 email_file.write(email+'\n')
             print(i+1, "Emails saved in '"+funnelname+".txt'")
             email_file.close()
+            return funnelname+'.txt'
             
     else:
         print('Something went wrong. Login not successful!')
+
+def get_all():
+    '''
+    Getting all e-mail addresses and save them in a single file <email.txt>
+    '''
+    ezmn_file = open(get_emails(funnel=EZMN), 'r')
+    ezmf_file = open(get_emails(funnel=EZMF), 'r')
+    emails = set()
+    for email in ezmn_file.readlines():
+        emails.add(email)
+    ezmn_file.close()
+    for email in ezmf_file.readlines():
+        emails.add(email)
+    ezmf_file.close()
+    email_file = open('emails.txt','w')
+    for i, email in enumerate(emails):
+        email_file.write(email)
+    email_file.close()
+    print(i+1, "unique email addresses stores in 'emails.txt'")
+    
         
 if __name__ == "__main__":
-    get_emails(funnel=5)
+    if len(argv) < 2:
+        print("""Usage: python3 ezmn_parser.py (FUNNEL)
+        
+        FUNNEL:
+            ezmn - for EZ Money Network Leads
+            ezmf - for EZ Money Formula Leads
+            ALL
+            
+        ------
+        Examples:
+            python3 ezmn_parser.py ezmn    -- Gets the Email addresses of your EZMN leads
+            python3 ezmn_parser.py ezmf    -- Gets the Email addresses of your EZMF leads
+            python3 ezmn_parser.py all     -- Getting leads from both funnels and store unique Emails in emails.txt
+        """)
+        exit(0)
+    if argv[1].upper() == 'EZMF': get_emails(funnel=EZMF);
+    elif argv[1].upper() == 'EZMN': get_emails(funnel=EZMN);
+    else: get_all()
